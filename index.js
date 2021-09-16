31b441650f6d92f009172053fa6ef3fd420b8fa9
viniflorencio <viniflorenciomid@gmail.com>
2021-09-16 01:36:57 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 
-const express = require('express');
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+
+
 const cors = require('cors');
+const express = require('express');
 const app = express();
 app.use(express.json());
 
+const corsOptions = {
+    origin: 'http://localhost:3001',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+  app.use(cors());
 
 const Conn = require('./models/conn/conn');
 
-Conn("localhost", 27017, "Tarefas");
 
-const port = 3002;
+const dbUrl = process.env.DB_URL;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS; 
+const dbData = process.env.DB_DATA;
+
+Conn(dbUrl, dbUser, dbPass, dbData);
+const port = 3000;
 
 const tarefa = require ('./routers/tarefas.routes');
 app.use('/tarefas', tarefa);
@@ -26,6 +42,6 @@ app.use('/tarefas', tarefa);
 
 
 
-app.listen(process.env.PORT || port,() =>{
+app.listen(port,() =>{
     console.info(`rodando na porta ${port}`);
 });
